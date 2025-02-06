4a24799d4bac66291234c2c5ec2ac483e065fae1
Mateus Pimenta <1920261+matpimenta@users.noreply.github.com>
2025-02-06 13:44:49 +0000

    Point to provider version 2.1 and run acceptance tests (#3)
    * Configure connection to GCP to run tests
    
    * Point to provider release v2.1
    
    * Update docs
    
    * Add timeout and parallel settings
    
    * Fix cache action and increase timeout

--- examples/active_active/versions.tf
+++ examples/active_active/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     rediscloud = {
       source  = "RedisLabs/rediscloud"
-      version = "~> 2.0" # TODO Bump to 2.1
+      version = "~> 2.1"
     }
     google = {
       source  = "hashicorp/google"
